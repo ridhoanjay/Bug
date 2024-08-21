160213d5a1d85f5ccfbb6d4b234cc3b9c16ee8e8
ridhoanjay <ardijuliansah111@gmail.com>
2024-08-21 20:55:46 +0700

    first commit

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6283897023533']
+global.botname = 'Kedys Bot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Rxhl Official"
+global.sticker2 = "🌜"
